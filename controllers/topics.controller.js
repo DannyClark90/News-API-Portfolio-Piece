de5b77fc088357f0760e58e423908e20f4194bdf
DannyClark90 <dannyclark_90@hotmail.com>
2023-11-20 15:53:18 +0000

    Re-structure of model & controller file system.

--- controllers/topics.controller.js
+++
@@ -1,8 +0,0 @@
-const { selectTopics } = require("../models/topics.model") //Import model
-
-exports.getTopics = (req, res) => {
-    selectTopics()
-    .then((topics) => {
-        res.status(200).send({topics})
-    })
-};
